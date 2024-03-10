166701641bd37be50d13291aabcf4fb9cd3422dd
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-03-10 15:23:39 -0400

    Initialized and set up tests

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-// ### User Story
-
-// ```md
-// AS a freelance web developer
-// I WANT to generate a simple logo for my projects
-// SO THAT I don't have to pay a graphic designer
-// ```
-
-// ## Acceptance Criteria
-
-// ```md
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-// ```
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateShapes = require("./lib/generateShapes.js");
-
 
 const questions = [
     
